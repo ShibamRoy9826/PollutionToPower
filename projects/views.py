e2c04295f4cbc1cc14a9117358fca0fd3d797625
ShibamRoy9826 <royshibam9826@gmail.com>
2024-06-30 22:48:39 +0530

    Completed Locality Report

--- projects/views.py
+++ projects/views.py
@@ -68,21 +68,21 @@ def carbonResults(request):
 
 
 def AQI(lat,lon):
-    url = f'http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={key}'   
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+	url = f'http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={key}'   
+	response = requests.get(url)
+	if response.status_code == 200:
+		return response.json()
+	else:
+		return None
 
 	
 def get_lat_lon(country, state, city):
-    geolocator = Nominatim(user_agent="air_quality_app")
-    location = geolocator.geocode(f"{city}, {state}, {country}")
-    if location:
-        return location.latitude, location.longitude
-    else:
-        return None
+	geolocator = Nominatim(user_agent="air_quality_app")
+	location = geolocator.geocode(f"{city}, {state}, {country}")
+	if location:
+		return location.latitude, location.longitude
+	else:
+		return None
 
 def localityReportResults(request):
 	try:
@@ -92,9 +92,43 @@ def localityReportResults(request):
 		city = request.GET["City"]
 		lat_lon = get_lat_lon(country, state, city)
 
-		data=AQI(lat_lon[0],lat_lon[1])
+		# data=AQI(lat_lon[0],lat_lon[1])
+		# Temporary
+		data={'coord': {'lon': 91.2824, 'lat': 23.8312}, 'list': [{'main': {'aqi': 1}, 'components': {'co': 317.1, 'no': 0, 'no2': 3.68, 'o3': 49.35, 'so2': 0.87, 'pm2_5': 8.91, 'pm10': 9.97, 'nh3': 1.74}, 'dt': 1719763015}]}
 
-		return render(request,"LocalityReport/LocalityReportResults.html",data)
+
+		dataMod={'co':data['list'][0]['components']['co'],
+		'no':data['list'][0]['components']['no'],
+		'no2':data['list'][0]['components']['no2'],
+		'o3':data['list'][0]['components']['o3'],
+		'so2':data['list'][0]['components']['so2'],
+		'pm2_5':data['list'][0]['components']['pm2_5'],
+		'pm10':data['list'][0]['components']['pm10'],
+		'nh3':data['list'][0]['components']['pm2_5']
+		}
+
+		components=data['list'][0]['components']
+		suggestions = []
+		if components['pm2_5'] > 45:
+			suggestions.append("The PM2.5 level is high. Consider wearing a mask if you go outside.")
+		if components['o3'] > 100:
+			suggestions.append("Ozone level is high. Reduce outdoor activities to avoid respiratory issues.")
+		if components['pm10'] > 255:
+			suggestions.append("PM10 level is high. Wear masks and avoid outdoor activitiesif possible.")
+
+		if components['co'] > 290:
+			suggestions.append("CO level is Hazardous⚠️! Please try to stay indoors. (Healthy: <12 µg/m³)")
+		elif components['co'] > 12:
+			suggestions.append("The CO level is quite high, Avoid traffic exposure if possible")
+		
+		if components['so2'] > 180:
+			suggestions.append("SO2 levels are quite high, make sure to wear masks, and avoid burining sulphur containing fuels")
+		if components['no2'] > 300:
+			suggestions.append("NO2 level is high, try to stay indoors")
+
+
+		dataMod["suggestions"]=suggestions
+		return render(request,"LocalityReport/LocalityReportResults.html",dataMod)
 
 	except Exception as e:
 		return HttpResponse(e)
